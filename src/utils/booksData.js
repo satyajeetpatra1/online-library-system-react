fbd2ca97312c48fcb01c2ecc8d584b897d383727
satyajeetpatra1 <satyajeetpatra.dev@gmail.com>
2025-12-30 00:01:23 +0530

    dummy book list created

---
+++ src/utils/booksData.js
@@ -0,0 +1,29 @@
+export const booksData = [
+  {
+    id: 1,
+    title: "Atomic Habits",
+    author: "James Clear",
+    category: "Non-Fiction",
+    description: "A guide to building good habits and breaking bad ones.",
+    rating: 4.8,
+    image: "https://images-na.ssl-images-amazon.com/images/I/91bYsX41DVL.jpg",
+  },
+  {
+    id: 2,
+    title: "Dune",
+    author: "Frank Herbert",
+    category: "Sci-Fi",
+    description: "Epic science fiction novel.",
+    rating: 4.6,
+    image: "https://images-na.ssl-images-amazon.com/images/I/91A0Zt1z7ZL.jpg",
+  },
+  {
+    id: 3,
+    title: "The Alchemist",
+    author: "Paulo Coelho",
+    category: "Fiction",
+    description: "A philosophical story about following dreams.",
+    rating: 4.5,
+    image: "https://images-na.ssl-images-amazon.com/images/I/71aFt4+OTOL.jpg",
+  },
+];
